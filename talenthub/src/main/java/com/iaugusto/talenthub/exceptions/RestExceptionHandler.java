5697ee5c57295a17178029e5d24915d6e013dea8
Igor Augusto <igoraugustobz@gmail.com>
2025-04-13 15:41:20 -0300

    :sparkles: feat: adicionar tratamento de exceções

---
+++ talenthub/src/main/java/com/iaugusto/talenthub/exceptions/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.iaugusto.talenthub.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> genericException(Exception ex) {
+        ApiError apiError = ApiError
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .errors(List.of(ex.getMessage()))
+                .build();
+
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
