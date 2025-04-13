13d5272acc5025d256781af8d6d9b03e3dc12607
Igor Augusto <igoraugustobz@gmail.com>
2025-04-13 14:49:53 -0300

    :tada: Commit inicial

---
+++ talenthub/src/main/java/com/iaugusto/talenthub/TalenthubApplication.java
@@ -0,0 +1,13 @@
+package com.iaugusto.talenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalenthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalenthubApplication.class, args);
+	}
+
+}
---
+++ talenthub/src/test/java/com/iaugusto/talenthub/TalenthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iaugusto.talenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalenthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
